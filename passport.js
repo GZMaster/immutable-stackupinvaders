5b449f6a2177a3a5cbf91d147ce97c21d4130f16
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-12-15 22:54:26 +0100

    changes quest complete

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
 window.passport = new window.immutable.passport.Passport({
-    baseConfig: new window.immutable.config.ImmutableConfiguration({
-      environment: window.immutable.config.Environment.SANDBOX,
-    }),
-    clientId: 'enter-client-id',
-    redirectUri: 'redirect-url',
-    logoutRedirectUri: 'logout-url',
-    audience: 'platform_api',
-    scope: 'openid offline_access email transact'
-  });
-
+	baseConfig: new window.immutable.config.ImmutableConfiguration({
+		environment: window.immutable.config.Environment.SANDBOX,
+	}),
+	clientId: "b2dL9n54SWrf4rpuYvnZILB8UOjYsW5K",
+	redirectUri: "https://safe-alpaca-vigorously.ngrok-free.app",
+	logoutRedirectUri:
+		"https://safe-alpaca-vigorously.ngrok-free.app/logout.html",
+	audience: "platform_api",
+	scope: "openid offline_access email transact",
+});
